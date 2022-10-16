5d44bb71801e318e2eba7a75d7deee45aadc4924
TheGamer1002 <48131223+TheGamer1002@users.noreply.github.com>
2022-10-16 16:20:28 -0400

    Added some more documentation, parameters,  fixes

--- Linkit.ps1
+++ Linkit.ps1
@@ -1,6 +1,13 @@
+## Handling arguments
+
+Param (
+    [Parameter(Position=0)][string]$Target,
+    [string]$Destination,
+    [switch]$CleanConfig,
+    [switch]$Quiet
+)
+
 # Linkit.ps1: Move a folder to a predefined location and create a symbolic link to it (to move folders to a different drive to save space)
-$folder = $args[0]; # The folder to move
-Write-Output $folder # for debugging purposes
 Add-Type -AssemblyName PresentationFramework # for the MessageBox
 <#
 .SYNOPSIS
@@ -17,90 +24,108 @@ linkit.ps1 "C:\Users\me\Downloads\myfolder"
 Moves the folder "myfolder" to a predefined location and creates a link to it in the original parent directory.
 
 .NOTES
-General notes
+EXIT CODES:
+0: Success
+1: No folder specified
+2: Folder does not exist or is not a folder
+3: Folder already exists in target location
+4: Could not create link
+5: Could not move folder
+6: User cancelled operation: Folder destination selection
+7: User cancelled operation: No config file created
+8: Config file is malformed or doesn't exist
 #>
 
 ## Functions: Create dialog boxes for errors and successes
 Function Get-error ($i) {
-    Write-Output "ERR: $i"
-    [System.Windows.MessageBox]::Show("Error: $i","Linkit", [System.Windows.MessageBoxButton]::OK, [System.Windows.MessageBoxImage]::Error)
+    if ($false -eq $Quiet) {
+        Write-Output "ERR: $i"
+        [System.Windows.MessageBox]::Show("Error: $i","Linkit", [System.Windows.MessageBoxButton]::OK, [System.Windows.MessageBoxImage]::Error)
+    }
 }
 Function fatal ($i) {
-    Write-Output "FATAL: $i"
-    [System.Windows.MessageBox]::Show("Fatal Error! $i","Linkit", [System.Windows.MessageBoxButton]::OK, [System.Windows.MessageBoxImage]::Error)
+    if ($false -eq $Quiet) {
+        Write-Output "FATAL: $i"
+        [System.Windows.MessageBox]::Show("Fatal Error! $i","Linkit", [System.Windows.MessageBoxButton]::OK, [System.Windows.MessageBoxImage]::Error)
+    }
 }
 Function success ($folder,$dest) {
-    Write-Output "INFO: Moved input folder $folder to $dest"
-    [System.Windows.MessageBox]::Show("Success: Moved input folder $folder to $dest", "Linkit", [System.Windows.MessageBoxButton]::OK, [System.Windows.MessageBoxImage]::Information)
+    if ($false -eq $Quiet) {
+        Write-Output "INFO: Moved input folder $folder to $dest"
+        [System.Windows.MessageBox]::Show("Success: Moved input folder $folder to $dest", "Linkit", [System.Windows.MessageBoxButton]::OK, [System.Windows.MessageBoxImage]::Information)
+    }
 }
 # I'm too lazy to call the function with its long name, so I create a shortcut
 Function wo ($i) {
-    Write-Output $i
+    if ($false -eq $Quiet) {
+        Write-Output $i
+    }
 }
 ## Read config file
-# Check if config file exists
-if (Test-Path -Path %appdata%\linkit\config.cfg) {
-    # Read config file
-    $config = Get-Content -Path %appdata%\linkit\config.cfg
+function Get-ConfigFile {
+    # Check if config file exists
+    if (Test-Path -Path %appdata%\linkit\config.cfg) {
+        # Read config file
+        $config = Get-Content -Path %appdata%\linkit\config.cfg
 
-    # Check if config file is empty
-    if ($null -eq $config) {
+        # Check if config file is empty
+        if ($null -eq $config) {
 
-        # Prompt for new config file
-        Write-Output "WARN: No config file found. Prompting for next steps..."
-        $Private:result = [System.Windows.MessageBox]::Show("No config file found. Create one now?","Linkit", [System.Windows.MessageBoxButton]::YesNo, [System.Windows.MessageBoxImage]::Question) -eq "Yes"
-        
-        if ($result) {
-            Write-Output "INFO: User chose to create a new config file"
-
-            # Create config file
-            wo "INFO: Creating config directory..."
-            New-Item -ItemType Directory -Path %appdata%\linkit -Force
-
-            wo "INFO: Creating config file..."
-            $config = New-Item -ItemType File -Path %appdata%\linkit\config.cfg -Force
-
-            # Prompt for destination
-            try {
-                wo "INFO: Prompting for destination..."
-                $dest = [System.Windows.Forms.FolderBrowserDialog]::new()
-                $dest.Description = "Select the destination folder"
-                $dest.ShowDialog()
-                $dest = $dest.SelectedPath
-            } catch {
-                fatal "User aborted or an error occured!"
-                exit # just exit the script. Don't want to break anything more
-            }
-            # Write destination to config file
-            wo "INFO: Writing destination to config file..."
-            $dest | Out-File -FilePath %appdata%\linkit\config.cfg
-
-            # Prompt for confirmation
-            $Private:result = [System.Windows.MessageBox]::Show("Config file created. Move folder now?","Linkit", [System.Windows.MessageBoxButton]::YesNo, [System.Windows.MessageBoxImage]::Question) -eq "Yes"
+            # Prompt for new config file
+            Write-Output "WARN: No config file found. Prompting for next steps..."
+            $Private:result = [System.Windows.MessageBox]::Show("No config file found. Create one now?","Linkit", [System.Windows.MessageBoxButton]::YesNo, [System.Windows.MessageBoxImage]::Question) -eq "Yes"
             
-        } else {
-            # Exit
-            Get-error "User chose not to create a new config file"
-            exit
-        }
-    }
-    # Check if config file has only one line
-    if ($config.count -ne 1) {
-        Get-error "Config file has more than one line"
-        exit
-    }
-    # Check if config file has a valid path
-    if (Test-Path -Path $config) {
-        $dest = $config
-    } else {
-        Get-error "Config file has an invalid path"
-        exit
-    }
-} else {
-    Get-error "Config file does not exist"
-    exit
-}
+            if ($result) {
+                Write-Output "INFO: User chose to create a new config file"
 
+                # Create config file
+                wo "INFO: Creating config directory..."
+                New-Item -ItemType Directory -Path %appdata%\linkit -Force
+
+                wo "INFO: Creating config file..."
+                $config = New-Item -ItemType File -Path %appdata%\linkit\config.cfg -Force
+
+                # Prompt for destination
+                try {
+                    wo "INFO: Prompting for destination..."
+                    $dest = [System.Windows.Forms.FolderBrowserDialog]::new()
+                    $dest.Description = "Select the destination folder"
+                    $dest.ShowDialog()
+                    $dest = $dest.SelectedPath
+                } catch {
+                    fatal "User aborted or an error occured!"
+                    exit 6 # just exit the script. Don't want to break anything more
+                }
+                # Write destination to config file
+                wo "INFO: Writing destination to config file..."
+                $dest | Out-File -FilePath %appdata%\linkit\config.cfg
+
+                # Prompt for confirmation
+                $Private:result = [System.Windows.MessageBox]::Show("Config file created. Move folder now?","Linkit", [System.Windows.MessageBoxButton]::YesNo, [System.Windows.MessageBoxImage]::Question) -eq "Yes"
+                
+            } else {
+                # Exit
+                Get-error "User chose not to create a new config file"
+                exit 7
+            }
+        }
+        # Check if config file has only one line
+        if ($config.count -ne 1) {
+            Get-error "Config file has more than one line"
+            exit 8
+        }
+        # Check if config file has a valid path
+        if (Test-Path -Path $config) {
+            $dest = $config
+        } else {
+            Get-error "Config file has an invalid path"
+            exit 8
+        }
+    } else {
+        Get-error "Config file does not exist"
+        exit 8
+    }
+}
 ## Move folder
 function Move-Folder ($folder) {
 
@@ -120,10 +145,11 @@ function Move-Folder ($folder) {
                         if ($result) { # User chose to move folder anyway
                             # Move folder
                             try {
-                                Move-Item -Path $folder -Destination $dest -Force
+                                start-process powershell.exe -argument "-nologo -noprofile -executionpolicy bypass -command Move-Item -Path $folder -Destination $dest -Force"
                                 success $folder $dest
                             } catch {
                                 fatal "An error occured while moving the folder: $_"
+                                exit 5
                             } # END try to move folder
                         } else { # User chose not to move folder
                             # Prompt for a workaround
@@ -140,42 +166,44 @@ function Move-Folder ($folder) {
 
                                 # Move folder
                                 try {
-                                    Move-Item -Path $folder -Destination $dest -Force
+                                    start-process powershell.exe -argument '-nologo -noprofile -executionpolicy bypass -command Move-Item -Path $folder -Destination $dest -Force'
                                     success $folder $dest
                                 } catch {
                                     fatal "An error occured while moving the folder: $_"
+                                    exit 5
                                 } # END try to move folder
 
                             } else { # User chose not to move folder to a symbolic parent directory
                                 # Exit
                                 Get-error "User chose not to move folder to a symbolic parent directory"
-                                exit
+                                exit 3
                             } # END if user chose to move folder to a symbolic parent directory
                         } #END prompt for confirmation due to non-empty destination
                     } else { # Folder can be moved safely (destination is empty)
                         # Move folder
                         try {
-                            Move-Item -Path $folder -Destination $dest -Force
+                            start-process powershell.exe -argument '-nologo -noprofile -executionpolicy bypass -command Move-Item -Path $folder -Destination $dest -Force'
                             success $folder $dest
                         } catch {
                             fatal "An error occured while moving the folder: $_"
+                            exit 5
                         } # END try to move folder
                     } # END check if destination is empty and move
                 } else { # Destination is not a directory
                     Get-error "Destination is not a directory"
-                    exit
+                    exit 2
                 } # END check if destination is a directory
             } else { # Destination does not exist
                 Get-error "Destination does not exist"
-                exit
+                exit 2
             } # END check if destination exists
         } else { # Input folder is not a directory
             Get-error "Input folder is not a directory"
-            exit
+            exit 2
         } # END check if input folder is a directory
     } else { # Input folder does not exist
         Get-error "Input folder does not exist"
-        exit
+        exit 2
     } # END check if input folder exists
     
 }
@@ -201,11 +229,12 @@ function New-Link ($folder,$dest) { # $folder is the folder to be linked, $dest 
                                 success $folder $dest
                             } catch {
                                 fatal "An error occured while creating the link: $_"
+                                exit 4
                             } # END try to create link
                         } else { # User chose not to create link
                             # Exit
-                            Get-error "User chose not to create the link"
-                            exit
+                            fatal "User chose not to create the link"
+                            exit 4
                         } #END prompt for confirmation due to non-empty destination
                     } else { # Link can be created safely (destination is empty)
                         # Create link
@@ -214,22 +243,24 @@ function New-Link ($folder,$dest) { # $folder is the folder to be linked, $dest 
                             success $folder $dest
                         } catch {
                             fatal "An error occured while creating the link: $_"
+                            exit 4
                         } # END try to create link
                     } # END check if destination is empty and create link
                 } else { # Destination is not a directory
                     Get-error "Destination is not a directory"
-                    exit
+                    exit 2
                 } # END check if destination is a directory
             } else { # Destination does not exist
                 Get-error "Destination does not exist"
-                exit
+                exit 2
             } # END check if destination exists
         } else { # Input folder is not a directory
             Get-error "Input folder is not a directory"
-            exit
+            exit 2
         } # END check if input folder is a directory
     } else { # Input folder does not exist
         Get-error "Input folder does not exist"
-        exit
+        exit 2
     } # END check if input folder exists
 }
+
